c9d226304ffe55ddc2c73c839439f627b3609646
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-07-09 14:00:43 -0600

    work from Friday

--- thurs.rb
+++ thurs.rb
@@ -25,7 +25,72 @@ end
 comp_app = napp | oapp
 puts comp_app
 
-###Get tables from each file:
+################################################################################
+#         # Things to ask first:  TODO
+################################################################################
+
+################################################################################
+#         # Declare strings
+################################################################################
+@cmd_s = ''
+
+t_s = 'table string'
+c_s = 'column string'
+a_s = 'attrib string'
+
+t_create = 'CREATE '
+t_modify = 'MODIFY '
+t_delete = 'DELETE '
+c_create = 'CREATE '
+c_modify = 'MODIFY '
+c_delete = 'DELETE '
+a_create = 'CREATE '
+a_modify = 'MODIFY '
+a_delete = 'DELETE '
+
+str_end = "\n);"
+
+
+######################################################################
+###---   Gets each attribute and outputs proper string fro Sql
+######################################################################
+#  @attrib = ''  @attrib_array = [undefined, undefined, undefined, undefined, undefined] <-- this needs to be stated right before this function is called.
+
+def string_case(name, attribute)
+  case name
+    when 'name'
+      attrib = attribute.value + " "
+      @array[0] = attrib
+    when 'sqltype'
+      attribute.value == 'INTEGER' ? attrib = 'INT ' : attrib = attribute.value
+      #if sqltype == "varchar", no space is needed after this attrib
+      # returns either INT or VARCHAR, VARCHAR is in this case so that it can be set without a space afterwords.
+      @array[1] = attrib
+    when 'size'
+      attrib = "(#{attribute.value}) "
+      #returns something like: (64), which is the size specification for VARCHAR
+      @array[2] = attrib
+    when 'auto_increment'
+      attrib = "IDENTITY(1,1) "
+      @array[3] = attrib
+    when 'nullable'
+      if attribute.value == 'yes' then
+        attrib = 'NULL DEFAULT '
+      end ## Check how to write if statement.....
+      @array[4] = attrib
+    when 'primarykey'
+      attribute.value == 'yes' ?  attrib = 'PRIMARY KEY ' : attrib = 'NULL'
+      @array[5] = attrib
+    when 'unsigned'
+      if attribute.value == 'yes' then  attrib = 'UNSIGNED ' end
+      @array << attrib
+    else
+      puts "From line 107"+" WARNING".red + " For column #{name} > #{attribute}=#{attribute.value} won't be set properly in SQL command.  Column '#{name}' hasn't been assiged a case yet. " + "*** ".red
+  end
+end
+################################################################################
+#         # Get table names from each file:
+################################################################################
 @new_tables = Array.new
 @old_tables = Array.new
 
@@ -38,72 +103,117 @@ end
   name = table.attribute("name").value
   @old_tables << name
 end
-## Function to get columns for input table from old file
+################################################################################
+#         # Function to get columns for input table from old file
+################################################################################
 @old_cols = Array.new
 def get_old_columns(n_table)
-
   this_table = @o_doc.at_css("Table[name='#{n_table}']")
   this_table.css("Column").each do |column|
     name = column.attribute("name").value
-    # print "***".light_blue + "***".light_cyan+ "***".light_blue, "old table named: ", name, "***".light_blue+"***".light_cyan+"***".light_blue+"\n"
-
     return @old_cols << name
-
   end
 end
 
-
-def get_attributes(file, old_table, old_column)
-
-  this_col = file.at_css("Table[name='#{old_table}']"&&"Column[name='#{old_column}']")
-  # p column
+################################################################################
+#         # Function to output string for a created column
+################################################################################
+def create_col_string(file, table_name, column_name)
+  this_col = file.at_css("Table[name='#{table_name}']"&&"Column[name='#{column_name}']")
   this_col.attributes.each do |name, attribute|
-    # hash = Hash.new
-    # hash[name] = attribute.value
-    @column_hash = @column_hash.merge!("#{name}" => "#{attribute.value}")
-    # print "\n**".green, name, " ", attribute.value, "**".green
-
-  end #end of old attribs
-  return @column_hash
+    string = ' '
+  # @attrib_array = [name, int/varchar, incrament, nullable?, is prim key?]
+    @array = ['undefined', 'undefined', 'undefined', 'undefined', 'undefined']
+    string_case(name, attribute)
+    puts @attrib
+    # string += @attrib
+    # @column_hash = @column_hash.merge!("#{name}" => "#{attribute.value}")
+  end
+  if (@array[5] == "PRIMARY KEY") then @array[4] = "NOT NULL " end
+  # @attrib = @array.join
+  # table_string += @attrib.chomp(" ") + ",\n  "
+  # return table_string#string  ####TODO RETURN STRING NOT HASH
 end
 
+################################################################################
+#         # Function to make string for a created table.
+################################################################################
+def create_table_string(file, table_name)
 
-###Compare both files:
+end
+
+################################################################################
+#         #
+################################################################################
 
 
-@string = ' '
-@error_msg = ' '
+
+################################################################################
+#         # Get all attributes of column.  Output Hash.
+################################################################################
+def get_attributes(file, table_name, column_name)
+  this_col = file.at_css("Table[name='#{table_name}']"&&"Column[name='#{column_name}']")
+  this_col.attributes.each do |name, attribute|
+    @column_hash = @column_hash.merge!("#{name}" => "#{attribute.value}")
+  end
+  return @column_hash
+end
+################################################################################
+#         # Compare both files:
+################################################################################
+@string = ''
+@alert_string = ''
+@error_msg = ''
 
 @n_doc.css("Application").each do |application|
   atters_count = 0
+
+  app_name = application.attribute("name").value
+  database = application.css("Database").attribute("name").value
+
   if napp.include?(application) && !oapp.include?(application) #string for create new app
-    @string += "Create new app '#{apps}'  "
-    #get all of contents from new app
+    @alert_string += "A new app: '#{app_name}' was created in the #{database} database. "
+    #TODO: get all of contents from new app and make better string
+
+  elsif oapp.include?(application) && !napp.include?(application)
+    @alert_string += "The app called '#{app_name}' was deleted from the #{database} database. "
+    #TODO: is it possible to add/delete app with MS-SQL?
+
   else #if app is in both new and old doc:
     application.css("Table").each do |table|
       table_name = table.attribute("name").value
-      # puts "\n***".yellow,table_name,"***".yellow
 
       if !@old_tables.include?(table_name) then
+        #TODO: make good string for new table created.
         @string += "\nCreate new table: #{table_name}  "
+        # ```
+        # CREATE TABLE
+        #   [ ]
+        #   ```
+
+      #elsif --TODO: table was deleted?
+
       elsif @old_tables.include?(table_name) then
         old_cols = get_old_columns(table_name) #gets columns for this table in old document
         # puts "#{table_name}".green
 
         table.css("Column").each do |column|
+          this_table_string = ''
           @column_hash = Hash.new #make new hash for "get attributes of columns" function
           column_name = column.attribute("name").value
 
           if !old_cols.include?(column_name) # new column was added
             #TODO: make good string to create this column
-            @string += "\nin  table #{table_name} Create new column: #{column_name} ".light_green
+            @string += "\nin  table #{table_name}".light_green
+            string = create_col_string(@n_doc, table_name, column_name)
+            # @string += string
           elsif old_cols.include?(column_name) # if both have column of same name
             #TODO: check col attributes, if old atters contain and match new atters, [X] get all attributes from old column
             #get attributes from old and new files as a hash:
             old_hash = get_attributes(@o_doc, table_name, column_name)
             new_hash = get_attributes(@n_doc, table_name, column_name)
             #compare atters:
-            if (old_hash.size) > (new_hash.size) #if an attrib was deleted TODO: write script for added/deleted attrib
+            if (old_hash.size) != (new_hash.size) #if an attrib was deleted TODO: write script for added/deleted attrib
               diff = old_hash - new_hash
               puts diff, "line 110".light_green
             elsif (new_hash.size) > (old_hash.size) #if an attrib was added TODO: write script for added/deleted attrib
@@ -119,7 +229,7 @@ end
               end #if old_s == new_s
             else
             end #if (old_hash.size) > (new_hash.size)
-          else puts column_name
+          else puts column_name #if column was deleted. TODO
 
           end #f !old_cols.include?(column_name)
 
@@ -130,6 +240,11 @@ end
     end #each do table
   end #if app stmnt
 end #each do app
+
+puts "***".light_green, @string, "***".light_green
+puts "***".light_magenta, @alert_string, "***".light_magenta
+puts "***".red, @error_msg, "***".red
+
 
 ##puts @string and error message:
 # puts @line.cyan, @string.green, @error_msg.red, @line.blue
