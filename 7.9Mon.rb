dc5f3af1fc31993f90356da4ade7f4d0dd8f6738
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-07-09 15:10:11 -0600

    end of day push

--- 7.9Mon.rb
+++ 7.9Mon.rb
@@ -110,7 +110,7 @@ def string_case(name, attribute)
       end ## Check how to write if statement.....
       @array[4] = attrib
     when 'primarykey'
-      attribute.value == 'yes' ?  attrib = 'PRIMARY KEY ' : attrib = 'NULL'
+      attribute.value == 'yes' ?  attrib = 'PRIMARY KEY' : attrib = 'NULL'
       @array[5] = attrib
     when 'unsigned'
       if attribute.value == 'yes' then  attrib = 'UNSIGNED ' end
@@ -118,7 +118,7 @@ def string_case(name, attribute)
     else
       puts "From line 107"+" WARNING".red + " For column #{name} > #{attribute}=#{attribute.value} won't be set properly in SQL command.  Column '#{name}' hasn't been assiged a case yet. " + "*** ".red
   end
-end
+ end
 ################################################################################
 #         # Get table names from each file:
 ################################################################################
@@ -152,29 +152,41 @@ end
 def create_col_string(file, table_name, column_name)
   puts table_name, column_name.blue
   this_col = file.at_css("Table[name='#{table_name}']"&&"Column[name='#{column_name}']")
-  puts this_col
+  @array = ['', '', '', 'NULL', 'NULL', '']
   this_col.attributes.each do |name, attribute|
-    puts name, attribute, "***".green
     string = ''
   # @attrib_array = [name, int/varchar, incrament, nullable?, is prim key?]
-    @array = ['undefined', 'undefined', 'undefined', 'undefined', 'undefined']
     string = string_case(name, attribute)
-    puts string
     # string += @attrib
     # @column_hash = @column_hash.merge!("#{name}" => "#{attribute.value}")
   end
-  if (@array[5] == "PRIMARY KEY") then @array[4] = "NOT NULL " end
+  if (@array[5] == "PRIMARY KEY") || (@array[5] != "NULL") then @array[4] = "NOT NULL " end
   string = @array.join
-  string = string.chomp(" ") + ",\n  "
-  print string
+  string = string.chomp(" ")
+  print "\n*** 168 ".cyan, string, "***".cyan
   return string #string  ####TODO RETURN STRING NOT HASH
 end
 
 ################################################################################
 #         # Function to make string for a created table.
 ################################################################################
-def create_table_string(file, table_name)
-
+def create_table_string(file, table_name, table)
+  this_table_string = ''
+  this_table_string += "CREATE TABLE IF NOT EXISTS #{table_name} (\n  "
+  col_string = ''
+  table.css("Column").each do |column|
+    @column_hash = Hash.new
+    column_name = column.attribute("name").value
+    string = create_col_string(@n_doc, table_name, column_name)
+    if col_string == '' then
+      col_string += string
+    else
+      col_string += ",\n  " + string
+    end
+  end #end of each col do:
+  this_table_string += col_string + "\n);\n"
+  # puts "\n****".light_green,  this_table_string, "****".light_green
+  return this_table_string
 end
 
 ################################################################################
@@ -219,18 +231,10 @@ end
       table_name = table.attribute("name").value
 
       if !@old_tables.include?(table_name) then
-        @cmd_s += "\nCREATE TABLE IF NOT EXISTS #{table_name} ("
-        this_table_string = ''
-        table.css("Column").each do |column|
-          @column_hash = Hash.new
-          column_name = column.attribute("name").value
-          string = ''
-          print "***** 203".green, @n_doc, table_name.magenta, column_name.magenta
-          string = create_col_string(@n_doc, table_name, column_name)
-          this_table_string += string
-        end
-        puts this_table_string
-      # TODO: elsif --TODO: table was deleted?
+        this_string = create_table_string(@n_doc, table_name, table)
+        puts "\n****".light_green,  this_string, "****".light_green
+
+      # TODO: elsif --TODO: table was deleted? -- not in this function.
       elsif @old_tables.include?(table_name) then
         old_cols = get_old_columns(table_name) #gets columns for this table in old document
         # puts "#{table_name}".green
